f153f84b1369f98ace1e3b02e8ac32b9d9c7c39f
Victor <vpellegrain@github.com>
2021-01-27 18:08:57 +0100

    changed local conf

--- examples/librispeech/s5/path.sh
+++ examples/librispeech/s5/path.sh
@@ -1,7 +1,7 @@
 export NEURALSP_ROOT=$PWD/../../../
 export TOOL=$NEURALSP_ROOT/tools/neural_sp
 export CONDA=$TOOL/miniconda
-export KALDI_ROOT=$TOOL/kaldi
+export KALDI_ROOT=$NEURALSP_ROOT/../kaldi
 
 # Kaldi
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
